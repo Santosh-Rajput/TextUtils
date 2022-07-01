bf73b6eba6d48b58419a259648ddd6f1fba65d9e
Santosh-Rajput <santoshrajput01sr@gmail.com>
2022-07-01 21:50:18 +0530

    h

---
+++ build/precache-manifest.788c1036f1926e984b47b4544fe3b351.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "54fa5721ef04b6b05e0e",
+    "url": "/UtlisText/static/css/main.4dad8491.chunk.css"
+  },
+  {
+    "revision": "54fa5721ef04b6b05e0e",
+    "url": "/UtlisText/static/js/main.54fa5721.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/UtlisText/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/UtlisText/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "93dd2573639504233547",
+    "url": "/UtlisText/static/js/runtime~main.93dd2573.js"
+  },
+  {
+    "revision": "89166acdcae809bfb4a43812a03339f5",
+    "url": "/UtlisText/index.html"
+  }
+];
