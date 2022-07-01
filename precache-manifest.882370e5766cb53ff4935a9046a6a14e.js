e854eb931ea2ffb35bb586e290894e9f3896c8f6
Santosh-Rajput <santoshrajput01sr@gmail.com>
2022-07-01 22:24:02 +0530

    Updates

---
+++ precache-manifest.882370e5766cb53ff4935a9046a6a14e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "54fa5721ef04b6b05e0e",
+    "url": "/TextUtlis/static/css/main.4dad8491.chunk.css"
+  },
+  {
+    "revision": "54fa5721ef04b6b05e0e",
+    "url": "/TextUtlis/static/js/main.54fa5721.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/TextUtlis/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/TextUtlis/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "4a73efcd2744a99b790d",
+    "url": "/TextUtlis/static/js/runtime~main.4a73efcd.js"
+  },
+  {
+    "revision": "58dc254bcab05c0b1f1bd7f5fb691c05",
+    "url": "/TextUtlis/index.html"
+  }
+];
